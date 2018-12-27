c5167404cd30a6b5d7cfe7c7dbba9df6fabd5266
DemoKnight <1123570680@qq.com>
2018-12-27 16:55:07 +0800

    初始化项目

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+//导入所需插件
+const gulp = require("gulp");
+const rename = require("gulp-rename");
+const uglify = require("gulp-uglify");
+const cssnano = require("gulp-cssnano");
+const sass = require("gulp-sass");
+const imagemin = require("gulp-imagemin");
+
+//发布任务
