da833e0cb25f8378783b76006185a701de30ad43
DemoKnight <1123570680@qq.com>
2018-12-28 22:43:19 +0800

    header部分大致完成

--- gulpfile.js
+++ gulpfile.js
@@ -7,3 +7,10 @@ const sass = require("gulp-sass");
 const imagemin = require("gulp-imagemin");
 
 //发布任务
+//压缩图片
+gulp.task('imagemin',function(){
+	gulp.src('./src/img/login/*')
+		.pipe(imagemin())
+		.pipe(rename({'suffix' : '.min'}))
+		.pipe(gulp.dest('./dist/img/login'))
+})
